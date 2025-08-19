50ba79b0fc6e2a1ba3ced4b23f79a103f51d3d03
Vitalii Pohorielov <vetal26101@gmail.com>
2025-08-19 10:38:41 +0400

    xxxcccxж\ч

--- script.js
+++ script.js
@@ -1,42 +1,35 @@
+function copyCode(code, button) {
+  navigator.clipboard.writeText(code).then(() => {
+    let original = button.innerText;
+    button.innerText = "Скопировано!";
+    button.disabled = true;
+    setTimeout(() => {
+      button.innerText = original;
+      button.disabled = false;
+    }, 2000);
+  });
+}
+
+function openLink(url) {
+  window.open(url, "_blank");
+}
+
+// Google авторизация
 function handleCredentialResponse(response) {
-  // Расшифровка JWT токена
   const data = parseJwt(response.credential);
-  console.log("Пользователь:", data);
 
-  document.querySelector("h1").innerText = "Добро пожаловать, " + data.name;
-  document.querySelector(".container").style.display = "flex";
-
-  // Скрываем кнопку входа
-  document.querySelector("#g_id_onload").style.display = "none";
-  document.querySelector(".g_id_signin").style.display = "none";
+  document.getElementById("user-info").classList.remove("hidden");
+  document.getElementById("user-name").innerText = data.name;
+  document.getElementById("user-pic").src = data.picture;
 }
 
 function parseJwt(token) {
   let base64Url = token.split('.')[1];
   let base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
   let jsonPayload = decodeURIComponent(
-    atob(base64)
-      .split('')
-      .map(function (c) {
-        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
-      })
-      .join('')
+    atob(base64).split('').map(function(c) {
+      return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+    }).join('')
   );
   return JSON.parse(jsonPayload);
-}
-
-function copyCode(button, code) {
-  navigator.clipboard.writeText(code).then(() => {
-    const originalText = button.innerText;
-    button.innerText = "Скопировано!";
-    button.disabled = true;
-    setTimeout(() => {
-      button.innerText = originalText;
-      button.disabled = false;
-    }, 2000);
-  });
-}
-
-function goToSite(url) {
-  window.open(url, "_blank");
 }
