d57d687509aa72eb6718032e60426ea509149ca2
Vitalii Pohorielov <vetal26101@gmail.com>
2025-08-19 10:45:01 +0400

    xxx

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Копирование кода
 function copyCode(code, button) {
   navigator.clipboard.writeText(code).then(() => {
     let original = button.innerText;
@@ -10,6 +11,7 @@ function copyCode(code, button) {
   });
 }
 
+// Переход по ссылке
 function openLink(url) {
   window.open(url, "_blank");
 }
@@ -18,16 +20,20 @@ function openLink(url) {
 function handleCredentialResponse(response) {
   const data = parseJwt(response.credential);
 
+  document.getElementById("auth-section").classList.add("hidden");
   document.getElementById("user-info").classList.remove("hidden");
+  document.getElementById("cards-section").classList.remove("hidden");
+
   document.getElementById("user-name").innerText = data.name;
   document.getElementById("user-pic").src = data.picture;
 }
 
+// Расшифровка JWT токена
 function parseJwt(token) {
   let base64Url = token.split('.')[1];
   let base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
   let jsonPayload = decodeURIComponent(
-    atob(base64).split('').map(function(c) {
+    atob(base64).split('').map(c => {
       return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
     }).join('')
   );
